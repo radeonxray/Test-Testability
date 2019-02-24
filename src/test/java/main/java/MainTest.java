0010fa85fee0047713e6830f460cdb079562a9b0
RadeonXRay <ceo_titanic@msn.com>
2019-02-24 23:52:28 +0100

    Minor additions

---
+++ src/test/java/main/java/MainTest.java
@@ -0,0 +1,28 @@
+package main.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+
+
+
+
+    @Test
+    void calculateTwoNumbers1() {
+
+    }
+
+    @Test
+    void calculateTwoNumbers() {
+    }
+
+    @Test
+    void calculateTwoNumbersTwo() {
+    }
+}
